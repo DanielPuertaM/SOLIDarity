cdbfc75caa692d76bb841474de2e5e490076c64e
DanielPuertaM <danielpuerta0926@gmail.com>
2025-11-18 00:44:13 -0500

    nuevas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,24 +3,26 @@ import { authGuard } from '@auth/guards/auth-guard';
 import { userGuard } from '@auth/guards/user-guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'auth',
-        pathMatch: 'full'
-    },
-    {
-        path: 'auth',
-        loadChildren: () => import('./auth/auth.routes'),
-        canActivateChild: [authGuard]
-    },
-    {
-        path: 'user',
-        loadChildren: () => import('./user/user.routes'),
-        canActivateChild: [userGuard]
-    },
-	{
-		path: '**',
-		redirectTo: 'auth'
-	}
-
+  {
+    path: '',
+    loadComponent: () => import('@shared/components/landing-page/landing-page'),
+  },
+   {
+    path: 'steps',
+    loadComponent: () => import('@shared/components/steps/steps'),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    canActivateChild: [authGuard],
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.routes'),
+    canActivateChild: [userGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConfirmModalComponent } from '@shared/components/alert-confirm/alert-confirm';
+import { AlertModalComponent } from '@shared/components/alert/alert';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AlertModalComponent,ConfirmModalComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
